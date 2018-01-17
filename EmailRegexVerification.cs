1af2edacc36f74c86689e119689a89529318e6f0
hristoil <h.chorbadzhiyski@gmail.com>
2018-01-17 13:18:19 +0200

    Fully working

---
+++ EmailRegexVerification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailRegexVerification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
+            string input = Console.ReadLine();
+
+            foreach (Match m in Regex.Matches(input, pattern))
+            {
+                Console.WriteLine("{0}", m.Value);
+            }
+        }
+    }
+}
